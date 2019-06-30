f1d2f3b7c2df7e56e101b72e15227d59a5102020
Hien Cao <hien.cao@aalto.fi>
2019-06-30 23:30:59 +0300

    finish unicafe step 3 && start unicafe step 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function Statistics(props) {
+	const [ good, neutral, bad ] = props.data;
+	return (
+		<div className="">
+			<h1>statistics</h1>
+			<p>{`good ${good}`}</p>
+			<p>{`neutral ${neutral}`}</p>
+			<p>{`bad ${bad}`}</p>
+			<p>{`all ${good + neutral + bad}`}</p>
+			<p>{`average ${good + neutral + bad ? (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad) : 0}`}</p>
+			<p>{`positive ${good ? good / (good + neutral + bad) * 100 : 0} %`}</p>
+		</div>
+	);
+}
+
 function App() {
 	const [ good, setGood ] = useState(0);
 	const [ neutral, setNeutral ] = useState(0);
@@ -12,13 +27,7 @@ function App() {
 			<button onClick={() => setGood(good + 1)}>good</button>
 			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
 			<button onClick={() => setBad(bad + 1)}>bad</button>
-			<h1>statistics</h1>
-			<p>{`good ${good}`}</p>
-			<p>{`neutral ${neutral}`}</p>
-			<p>{`bad ${bad}`}</p>
-			<p>{`all ${good + neutral + bad}`}</p>
-			<p>{`average ${good + neutral + bad ? (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad) : 0}`}</p>
-			<p>{`positive ${good ? good / (good + neutral + bad) * 100 : 0} %`}</p>
+			<Statistics data={[ good, neutral, bad ]} />
 		</div>
 	);
 }
