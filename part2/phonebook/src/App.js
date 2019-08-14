061974307a147e0a993ff2931f6a5f98ce7b0419
Hien Cao <hien.cao@wartsila.com>
2019-08-14 15:42:59 +0300

    finish Phonebook Step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [ persons, setPersons ] = useState([ { name: 'Arto Hellas' } ]);
+  const [ newName, setNewName ] = useState('');
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setPersons([ ...persons, { name: newName } ]);
+          setNewName('');
+        }}
+      >
+        <div>
+          name: <input onChange={(e) => setNewName(e.target.value)} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+    </div>
+  );
+};
+
+export default App;
