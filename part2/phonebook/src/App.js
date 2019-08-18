7b7015408c823f8bf13c67df64df8ff27d9b9568
Hien Cao <hien.cao@aalto.fi>
2019-08-18 12:01:23 +0300

    finish the Phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
+	/* const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  ]); */
 
-  const [ filter, setFilter ] = useState('');
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={filter} setFilter={setFilter} />
-      <h2>Add a new</h2>
-      <PersonForm persons={persons} setPersons={setPersons} />
-      <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
-    </div>
-  );
+	const [ persons, setPersons ] = useState([]);
+	const [ filter, setFilter ] = useState('');
+
+	useEffect(() => {
+		axios.get('http://localhost:3001/persons').then((response) => setPersons(response.data));
+	}, []);
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<Filter filter={filter} setFilter={setFilter} />
+			<h2>Add a new</h2>
+			<PersonForm persons={persons} setPersons={setPersons} />
+			<h2>Numbers</h2>
+			<Persons persons={persons} filter={filter} />
+		</div>
+	);
 };
 
 export default App;
